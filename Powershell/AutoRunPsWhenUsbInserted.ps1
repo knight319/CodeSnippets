a231c9fb49dd98e9ca241a3660257fe01b72e884
Knight <knight.chan319@gmail.com>
2023-02-12 03:34:19 +0800

    Add progress for copying/moving

---
+++ Powershell/AutoRunPsWhenUsbInserted.ps1
@@ -0,0 +1,14 @@
+﻿# Not work as expected, it only works when the powershell session is active.
+
+# Unregister the existing WMI event
+Unregister-Event -SourceIdentifier "Win32_LogicalDiskInsertEvent"
+
+# Re-register the WMI event with the updated action
+$Query = "select * from __InstanceCreationEvent within 5 where TargetInstance ISA 'Win32_LogicalDisk' and TargetInstance.DriveType = 2"
+
+# Register the WMI event query
+$WMI = Register-WMIEvent -Query $Query -SourceIdentifier "Win32_LogicalDiskInsertEvent" -Action {
+  # Get the drive letter of the USB drive that was just inserted
+  $DriveLetter = ($Event.SourceEventArgs.NewEvent.TargetInstance.Name)
+  ("[" + (Get-Date).ToString('O') + "] USB drive with letter $DriveLetter was inserted") >> (Join-Path $env:USERPROFILE "Win32_LogicalDiskInsertEvent.txt")
+}
--- Powershell/MoveDcim2NasV2.ps1
+++ Powershell/MoveDcim2NasV2.ps1
@@ -1,5 +1,61 @@
 ﻿## this need the sync.exe tool in SysinternalsSuite:https://learn.microsoft.com/en-us/sysinternals/downloads/sysinternals-suite
 
+param (
+  # [Parameter(Mandatory=$true, ParameterSetName="Set1")]
+  [string]$DriveLetter = "",
+  [string]$LogPath = "$env:userprofile\MoveDcim2NasV2.log"
+)
+
+Function Write-Log($str) {
+    $logstr = ("[" + (Get-Date).ToString('O') + "] $str")
+    Write-Host $logstr
+    $logstr >> $LogPath
+}
+
+function Convert-ByteSize {
+  param(
+    [double]$Bytes
+  )
+  $Units = @("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+  $Unit = 0
+  while ($Bytes -ge 1KB) {
+    $Bytes /= 1KB
+    $Unit++
+  }
+  [string]::Format("{0:0.##} {1}", $Bytes, $Units[$Unit])
+}
+
+function CopyItemWithSpeed([string]$source, [string]$destination, [bool]$move = $false) {
+  if ($move -and ($source.ToLower()[0] -eq $destination.ToLower()[0])) { Move-Item $source $destination }
+
+  try {
+      $fileSize = (Get-Item $source).Length
+      $bytesCopied = 0
+
+      $copy = [System.IO.File]::OpenRead($source)
+      $dest = [System.IO.File]::Create($destination)
+
+      $buffer = New-Object byte[] 1048576
+      $fileSizeHumanReadable = Convert-ByteSize $fileSize
+      while ($bytesCopied -lt $fileSize)
+      {
+          $read = $copy.Read($buffer, 0, $buffer.Length)
+          if ($read -eq 0) { break }
+          $dest.Write($buffer, 0, $read)
+
+          $bytesCopied += $read
+          $bytesCopiedHumanReadable = Convert-ByteSize $bytesCopied
+          $percentComplete = [int](($bytesCopied / $fileSize) * 100)
+          Write-Progress -Id 99999 -Activity "Copying $source to $destination" -PercentComplete $percentComplete -Status "$bytesCopiedHumanReadable of $fileSizeHumanReadable bytes copied"
+      }
+      if ($move) { Remove-Item $source }
+  }
+  finally {
+      $copy.Close()
+      $dest.Close()
+  }
+}
+
 Function CopyOrMoveBasedOnDate($src, $dest, $move=$false){
     # op1: use robocopy to copy, it is faster but cannot move file to it's related folder based on CreatedDate, like 2023-02
     # copy and move
@@ -9,42 +65,54 @@ Function CopyOrMoveBasedOnDate($src, $dest, $move=$false){
     # op2: Get-ChildItem and Move-Item. Move items to CreatedDate related folder, like 2023-02
     $cnt = 0;
     $items = Get-ChildItem $src -Recurse -File
-    Write-Host "Total" $items.Count "files."
+    Write-Log ("Total " + $items.Count + " files.")
     $items | % {
         $dir = Join-Path $dest $_.CreationTime.ToString("yyyy-MM");
         If (!(Test-Path $dir)){ mkdir $dir };
         $destfilepath = Join-Path $dir $_.Name;
-        If ($move) { Move-Item $_.FullName $destfilepath -Force; }
-        ELSE { Copy-Item $_.FullName $destfilepath -Force; }
+        
         $cnt += 1;
         $pct = ($cnt/($items.Count))*100
-        Write-Progress -Activity "Copy to $dest" -Status "Processing $cnt. Percent complete" -PercentComplete $pct
+        Write-Progress -Id 1001 -Activity "Copy to $dest" -Status "Processing $cnt files. Percent complete" -PercentComplete $pct
+        If ($move) { 
+            # Move-Item $_.FullName $destfilepath -Force;
+            CopyItemWithSpeed $_.FullName $destfilepath $true
+        }
+        ELSE {
+            # Copy-Item $_.FullName $destfilepath -Force; 
+            CopyItemWithSpeed $_.FullName $destfilepath $false
+        }
     }
     
     # Delete empty folders in source.
     If ($move) { robocopy $src $src /s /move; }
-    Write-Host "Done $cnt files."
+    Write-Log ("Done " + $cnt + " files.")
 }
 
-Function DetectDcimFolderInUseDisk(){
+Function DetectDcimFolderInUseDisk($DriveLetter){
+    if ($DriveLetter) {
+        $tmp_p = Join-Path $disk.DriveLetter "DCIM"
+        if (Test-Path $tmp_p) { return $tmp_p }
+    }
+
     $src = ""
     $disk = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 2 }
     if ($disk.GetType().Name -eq 'ManagementObject') {
         $tmp_p = Join-Path $disk.DriveLetter "DCIM"
         if (Test-Path $tmp_p) { $src = $tmp_p }
     } elseif (!$disk) {
-        Write-Error "No USB disk found. Exiting"
+        Write-Log "No USB disk found. Exiting"
         Pause
         return $src;
     } else {
-        Write-Error "More than 1 USB disk found. Exiting"
+        Write-Log "More than 1 USB disk found. Exiting"
         Pause
         return $src;
     }
     return $src
 }
 
-$src = DetectDcimFolderInUseDisk
+$src = DetectDcimFolderInUseDisk $DriveLetter
 
 $dest = "$env:userprofile\Pictures\Back2Nas\FujiXS10\"
 $destDate = Join-Path $dest (Get-Date).ToString("yyyy-MM")
